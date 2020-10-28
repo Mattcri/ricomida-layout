8b726ab0e8c1aa2cdddd8c4c2343d0f17c13377a
Mcriado <mcriado.cg@gmail.com>
2020-10-28 18:36:54 -0300

    Ricomida layout with Bootstrap

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
+
+  function favoritos() {
+      alert('La receta se agrego a su sección de favoritos');
+  }
+
+  function correo() {
+      alert('La receta se envío a su correo');
+  }
